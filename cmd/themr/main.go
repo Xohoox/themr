6f8201663a38b57dcb6c4dfc40688a39f274d4ad
Fynn Michell-Auli <f.michell-auli@live.de>
2026-01-30 17:40:03 +0100

    help menu

--- cmd/themr/main.go
+++ cmd/themr/main.go
@@ -6,18 +6,59 @@ import (
 	"github.com/xohoox/themr/pkg/themr"
 )
 
+func printUsageScreenProfile() {
+	fmt.Println("Commands:")
+	fmt.Printf("  screenProfile                   Manage ScreenProfiles\n")
+	fmt.Printf("    list                          List all ScreenProfiles\n")
+	fmt.Printf("    select <name>                 Apply the <name> xrandr configuration\n")
+	fmt.Printf("    addCurrent <name>             Add current xrandr configuration as <name> to themr\n")
+	fmt.Printf("    rename <oldName> <newName>    Rename a ScreenProfile from <oldName> to <newName>\n\n")
+}
+
+func printUsageWallpaper() {
+	fmt.Printf("  wallpaper                       Manage wallpapers\n")
+	fmt.Printf("    list                          List all wallpaper\n")
+	fmt.Printf("    select <name>                 Apply the <name> wallpaper group\n")
+	fmt.Printf("    rename <oldName> <newName>    Rename a wallpaper from <oldName> to <newName>\n\n")
+}
+
+func printUsage() {
+	fmt.Printf("usage: themr [-h | --help] <command> [args]\n\n")
+	printUsageScreenProfile()
+	printUsageWallpaper()
+}
+
 func main() {
 
 	if err := themr.ReadConfig(); err != nil {
 		fmt.Println(err)
+		os.Exit(1)
 	}
 
-	if len(os.Args) < 2 {
-		fmt.Println("expected 'screenProfile', 'theme' or 'wallpaper' as subcommand! Use --help for more information")
+	if len(os.Args) <= 2 {
+		printUsage()
 		os.Exit(1)
 	}
 
 	switch os.Args[1] {
+	case "-h", "--help":
+		if len(os.Args) == 2 {
+			printUsage()
+			os.Exit(1)
+		}
+
+		switch os.Args[2] {
+		case "screenProfile":
+			fmt.Printf("usage: themr [-h | --help] screenProfile [args]\n\n")
+			printUsageScreenProfile()
+		case "wallpaper":
+			fmt.Printf("usage: themr [-h | --help] wallpaper [args]\n\n")
+			printUsageWallpaper()
+		default:
+			printUsage()
+		}
+		os.Exit(1)
+
 	case "screenProfile":
 		if len(os.Args) == 2 {
 			fmt.Println("Unknown command! Use --help for more information")
@@ -101,7 +142,7 @@ func main() {
 			os.Exit(1)
 		}
 	default:
-		fmt.Printf("Unknown subcommand: %s! Use --help for more information\n", os.Args[1])
+		printUsage()
 		os.Exit(1)
 	}
 }
