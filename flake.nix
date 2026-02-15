552026f456daf754b30737259bc674180248d83c
Fynn Michell-Auli <f.michell-auli@live.de>
2026-02-15 20:30:03 +0100

    nix flake

---
+++ flake.nix
@@ -0,0 +1,19 @@
+{
+  description = "Control Themes Monitors and Wallpaper on X using xrandr and xwallpaper";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+
+  outputs = { self, nixpkgs }:
+  let
+    system = "x86_64-linux";
+    pkgs = import nixpkgs { inherit system; };
+  in {
+    packages.${system}.default = pkgs.buildGoModule {
+      pname = "themr";
+      version = "0.1.0";
+      src = self;
+      subPackages = [ "cmd/themr" ];
+      vendorHash = null;
+    };
+  };
+}
